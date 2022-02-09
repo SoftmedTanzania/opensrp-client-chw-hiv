a5462242706da646715a848b10475eee64d7d104
Billy <billypatrick767@gmail.com>
2022-02-09 10:22:37 +0300

    refactored code

--- opensrp-chw-hiv/src/main/java/org/smartregister/chw/hiv/activity/BaseHivFormsActivity.kt
+++ opensrp-chw-hiv/src/main/java/org/smartregister/chw/hiv/activity/BaseHivFormsActivity.kt
@@ -1,6 +1,5 @@
 package org.smartregister.chw.hiv.activity
 
-import android.app.Activity
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
@@ -32,7 +31,6 @@ import org.smartregister.chw.hiv.util.DBConstants
 import org.smartregister.chw.hiv.util.JsonFormConstants
 import org.smartregister.view.activity.SecuredActivity
 import timber.log.Timber
-import java.util.*
 
 /**
  * Created by cozej4 on 2020-05-13.
@@ -104,68 +102,75 @@ open class BaseHivFormsActivity : SecuredActivity(), BaseHivFormsContract.View {
                     jsonForm?.getJSONArray("steps")?.getJSONObject(0)?.getString("title") ?: ""
             }
 
-            exitFormImageView.setOnClickListener {
-                if (it.id == R.id.exitFormImageView) {
-                    AlertDialog.Builder(
-                        this@BaseHivFormsActivity,
-                        R.style.AlertDialogTheme
-                    )
-                        .setTitle(getString(R.string.confirm_form_close))
-                        .setMessage(getString(R.string.confirm_form_close_explanation))
-                        .setNegativeButton(R.string.yes) { _: DialogInterface?, _: Int -> finish() }
-                        .setPositiveButton(R.string.no) { _: DialogInterface?, _: Int ->
-                            Timber.d("Do Nothing exit confirm dialog")
-                        }
-                        .create()
-                        .show()
-                }
-            }
+            exitFormImageView.setOnClickListener(this@BaseHivFormsActivity::onClick)
 
-            completeButton.setOnClickListener {
-                if (it.id == R.id.completeButton) {
-                    if (formBuilder?.getFormDataAsJson() != "") {
-
-                        val formData = formBuilder!!.getFormData()
-                        if (formData.isNotEmpty()) {
-
-                            if (jsonForm!!.getString(JsonFormConstants.ENCOUNTER_TYPE) == Constants.EventType.HIV_COMMUNITY_FOLLOWUP_FEEDBACK) {
-                                //Saving referral form id
-                                formData[DBConstants.Key.COMMUNITY_REFERRAL_FORM_ID] =
-                                    NFormViewData().apply {
-                                        value = hivMemberObject!!.communityReferralFormId
-                                    }
-
-                                //Saving chw names
-                                val allSharedPreferences = hivLibrary.context.allSharedPreferences()
-                                formData[DBConstants.Key.CHW_NAME] =
-                                    NFormViewData().apply {
-                                        value = allSharedPreferences.getANMPreferredName(
-                                            allSharedPreferences.fetchRegisteredANM()
-                                        )
-                                    }
-                            }
-
-                            presenter!!.saveForm(formData, jsonForm!!)
-                            Timber.d("Saved Data = %s", Gson().toJson(formData))
-                            val intent = Intent()
-                            setDataToBePassedBackToCallingActivityAsResults(
-                                intent,
-                                jsonForm!!,
-                                formData
-                            )
-                            setResult(Activity.RESULT_OK, intent)
-                            finish()
-                        }
-
-
-
-                        finish()
-                    }
-                }
-            }
+            completeButton.setOnClickListener(this@BaseHivFormsActivity::onClick)
 
             createViewsFromJson()
         }
+    }
+
+    private fun onClick(it: View) {
+        if (it.id == R.id.exitFormImageView) {
+            showAlertDialog()
+        }
+        if (it.id == R.id.completeButton) {
+            saveForm()
+        }
+    }
+
+    private fun saveForm() {
+        if (formBuilder?.getFormDataAsJson() != "") {
+            val formData = formBuilder!!.getFormData()
+            if (formData.isNotEmpty()) {
+                if (jsonForm!!.getString(JsonFormConstants.ENCOUNTER_TYPE) == Constants.EventType.HIV_COMMUNITY_FOLLOWUP_FEEDBACK) {
+                    //Saving referral form id
+                    processReferralFeedbackForm(formData)
+                }
+                presenter!!.saveForm(formData, jsonForm!!)
+                Timber.d("Saved Data = %s", Gson().toJson(formData))
+                val intent = Intent()
+                setDataToBePassedBackToCallingActivityAsResults(
+                    intent,
+                    jsonForm!!,
+                    formData
+                )
+                setResult(RESULT_OK, intent)
+                finish()
+            }
+            finish()
+        }
+    }
+
+    private fun processReferralFeedbackForm(formData: HashMap<String, NFormViewData>) {
+        formData[DBConstants.Key.COMMUNITY_REFERRAL_FORM_ID] =
+            NFormViewData().apply {
+                value = hivMemberObject!!.communityReferralFormId
+            }
+
+        //Saving chw names
+        val allSharedPreferences = hivLibrary.context.allSharedPreferences()
+        formData[DBConstants.Key.CHW_NAME] =
+            NFormViewData().apply {
+                value = allSharedPreferences.getANMPreferredName(
+                    allSharedPreferences.fetchRegisteredANM()
+                )
+            }
+    }
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(
+            this@BaseHivFormsActivity,
+            R.style.AlertDialogTheme
+        )
+            .setTitle(getString(R.string.confirm_form_close))
+            .setMessage(getString(R.string.confirm_form_close_explanation))
+            .setNegativeButton(R.string.yes) { _: DialogInterface?, _: Int -> finish() }
+            .setPositiveButton(R.string.no) { _: DialogInterface?, _: Int ->
+                Timber.d("Do Nothing exit confirm dialog")
+            }
+            .create()
+            .show()
     }
 
     override fun onResumption() = Unit
